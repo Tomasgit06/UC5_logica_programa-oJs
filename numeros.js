d3f2bbd5e6b1809ca1c42a5e81f1defafec74960
Tomasgit06 <cabraltomas297@gmail.com>
2026-08-30 22:34:29 -0300

    Delete numeros.js

--- numeros.js
+++
@@ -1,6 +0,0 @@
-const prompt = require("prompt-sync")()
-
-let numero1 = Number(prompt ("Digite um numero:"));
-let numero2 = Number(prompt ("Digite um numero:"));
-
-console.log (numero1 + numero2);
