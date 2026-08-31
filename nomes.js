ae5c87f8121f8fac85bed4aaed1547cd81e895e7
Tomasgit06 <cabraltomas297@gmail.com>
2026-08-30 22:34:20 -0300

    Delete nomes.js

--- nomes.js
+++
@@ -1,7 +0,0 @@
-const prompt = require("prompt-sync")()
-
-let nome = prompt("Digite seu nome:")
-let idade = prompt("digite sua idade:")
-let endereco = prompt("digite seu endereco:")
-
-console.log("cadastro salvo o seu nome e: " + (nome) + " tem " + (idade) + " anos " + "seu endereço e:" + (endereco) )
